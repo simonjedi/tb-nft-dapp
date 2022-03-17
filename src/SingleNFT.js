9d3d2a6e5f82ada9ecc49d225f7389f8b51e3dd8
Jedi <getfit121.com@googlemail.com>
2022-03-17 19:00:13 +0000

    update

--- src/SingleNFT.js
+++ src/SingleNFT.js
@@ -24,12 +24,12 @@ const SingleNFT = (props) => {
   // Balance of NFTs
 
   // Transform Json Element
-  // var trait = props.item.attributes[0].trait_type.split("_")
-  // if (trait[1]) {
+  var trait = props.item.attributes[0].trait_type.split("_")
+  if (trait[1]) {
     
-  // } else {
-  //   trait[1] = ""
-  // }
+  } else {
+    trait[1] = ""
+  }
   
   // console.log(props.item.image,"image")
   // console.log(props.nft_balanceOf[props.id],"bal")
@@ -37,19 +37,19 @@ const SingleNFT = (props) => {
 // Not used as each one is a whole NFT
   // balanceOfresult = props.nft_balanceOf.substring(0, tokenContract_xyz_.length-18)
   
-  var base_url = "https://api.treasureblox.finance/"
-  var json_url = props.item.image.split("https://api.treasureblox.finance/");
+  // var base_url = "https://api.treasureblox.finance/"
+  // var json_url = props.item.image.split("https://api.treasureblox.finance/");
 
-  var final_img = base_url.concat(json_url[1]);  
+  // var final_img = base_url.concat(json_url[1]);  
 
-  console.log(final_img,"final_img")
-
+  console.log(props.item.image,"final_img")
+  
   return(
     <div >
 
     {(bal >= 1)?(
       <div>
-          <Card.Img variant="top"  src={final_img} key={props.item.image} alt="Logo" className='customRounded nftImg'/>
+          <Card.Img variant="top"  src={props.item.image} key={props.item.image} alt="Logo" className='customRounded nftImg'/>
             {/* <div className="nftTitle">Name: {props.item.name}</div>
             <div className="nftSubTitle">TRAIT TYPE: {trait[0].toUpperCase()} {trait[1].toUpperCase()}</div>
             <div className="nftSubTitle">ITEM DESCRIPTION: {props.item.attributes[0].value}</div>
