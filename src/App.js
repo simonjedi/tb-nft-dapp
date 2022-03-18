8ffadaff6f6111a6f126be36dcb65daac0ef98ff
Jedi <getfit121.com@googlemail.com>
2022-03-18 09:22:43 +0000

    update

--- src/App.js
+++ src/App.js
@@ -325,7 +325,7 @@ const App = (props) => {
           nft_metadata = nft_metadata.split("https://api.treasureblox.finance/");
           // console.log(nft_metadata[1],"nft_metadata")
 
-           fetch(nft_metadata[1],{
+           fetch(nft_metadata,{
                     headers : { 
                       'Content-Type': 'application/json',
                       'Accept': 'application/json'
