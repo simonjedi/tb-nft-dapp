8079b2eec0414b327c840df731928943fc1bf272
Jedi <getfit121.com@googlemail.com>
2022-03-18 09:55:39 +0000

    update

--- src/App.js
+++ src/App.js
@@ -325,7 +325,7 @@ const App = (props) => {
           nft_metadata = nft_metadata.split("https://api.treasureblox.finance/");
           // console.log(nft_metadata[1],"nft_metadata")
 
-           fetch("https://api.treasureblox.finance/v1/LootBox_MetaData/GOLD/BLUE_EYES.json",{
+           fetch(nft_metadata[1],{
                     headers : { 
                       'Content-Type': 'application/json',
                       'Accept': 'application/json'
